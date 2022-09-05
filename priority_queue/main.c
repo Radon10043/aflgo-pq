e6231747bbd82213b6d46dd3162ca9374573dcf4
Radon10043 <772577952@qq.com>
2022-09-05 17:14:33 +0800

    Added: Comment

--- priority_queue/main.c
+++ priority_queue/main.c
@@ -2,13 +2,15 @@
  * @Author: Radon
  * @Date: 2022-09-05 16:50:04
  * @LastEditors: Radon
- * @LastEditTime: 2022-09-05 17:11:05
+ * @LastEditTime: 2022-09-05 17:14:11
  * @Description: Hi, say something
  */
 #include "priority_queue.h"
 
 int main(int argc, char **argv) {
   s32 arr[5] = {5, 4, 3, 2, 1};
+
+  /* 目前是数小的元素在队首 (堆顶) */
 
   priority_queue *pq = (priority_queue *)malloc(sizeof(priority_queue));
   init_pqueue(pq);
